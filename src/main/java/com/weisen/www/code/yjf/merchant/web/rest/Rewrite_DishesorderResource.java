5bf62984a0801ae0a0b06f48fb6c2b925ad8399c
Daniey <lixin520gj@163.com>
2019-07-04 09:22:39 +0800

    rebuild

--- src/main/java/com/weisen/www/code/yjf/merchant/web/rest/Rewrite_DishesorderResource.java
+++ src/main/java/com/weisen/www/code/yjf/merchant/web/rest/Rewrite_DishesorderResource.java
@@ -9,7 +9,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +43,23 @@ public class Rewrite_DishesorderResource {
     @ApiOperation("创建菜单")
     public ResponseEntity<DishesorderDTO> createMenu (@RequestBody Rewrite_CreateMenuDTO createMenuDTO) {
         return dishesorderService.createMenu(createMenuDTO);
+    }
+    
+	//测试接口(增删改查)
+	@PostMapping("/test")
+    public String createTest(@RequestBody String name) {
+        return "调用了MERCHANT的增加接口";
+    }
+    @DeleteMapping("/test/{name}")
+    public String deleteTest(@PathVariable String name) {
+        return "调用了MERCHANT的删除接口";
+    }
+    @PutMapping("/test")
+    public String updateTest(@RequestBody String name) {
+        return "调用了MERCHANT的修改接口";
+    }
+    @GetMapping("/test/{name}")
+    public String getTest(@PathVariable String name) {
+        return "调用了MERCHANT的查询接口";
     }
 }
