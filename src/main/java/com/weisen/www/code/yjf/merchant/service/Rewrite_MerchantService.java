30a15135b6480c4e3fbf1297a9322622c9b0639b
jinshui <1511328878@qq.com>
2019-10-31 14:40:12 +0800

    修改返回值

--- src/main/java/com/weisen/www/code/yjf/merchant/service/Rewrite_MerchantService.java
+++ src/main/java/com/weisen/www/code/yjf/merchant/service/Rewrite_MerchantService.java
@@ -11,33 +11,33 @@ import com.weisen.www.code.yjf.merchant.service.util.Result;
 
 public interface Rewrite_MerchantService {
 
-    //添加商家店铺
-    String createMerchant(MerchantDTO merchantDTO);
+	// 添加商家店铺
+	String createMerchant(MerchantDTO merchantDTO);
 
-    //修改店铺信息
-    void updateMerchant(Rewrite_MerchantDTO merchantDTO);
+	// 修改店铺信息
+	Result updateMerchant(Rewrite_MerchantDTO merchantDTO);
 
-    //查询我的店铺
-    MerchantDTO findMyShop(Long userid);
+	// 查询我的店铺
+	MerchantDTO findMyShop(Long userid);
 
-    //查询店铺信息
-    MerchantDTO findShopInfo(Long userid);
+	// 查询店铺信息
+	MerchantDTO findShopInfo(Long userid);
 
-    //查询附近热门店铺
-    List<MerchantDTO> findPopularMerchant(Rewrite_ForNearShop rewrite_ForNearShop);
+	// 查询附近热门店铺
+	List<MerchantDTO> findPopularMerchant(Rewrite_ForNearShop rewrite_ForNearShop);
 
-    //距离最近的店铺
-    List<MerchantDTO> findNearMerchant(Rewrite_ForNearShop rewrite_ForNearShop);
+	// 距离最近的店铺
+	List<MerchantDTO> findNearMerchant(Rewrite_ForNearShop rewrite_ForNearShop);
 
-    //根据搜索内容查询商户
-    Result findByNameLike(Rewrite_ForNearShop rewrite_ForNearShop);
+	// 根据搜索内容查询商户
+	Result findByNameLike(Rewrite_ForNearShop rewrite_ForNearShop);
 
-    // 分页倒叙查询商家
-    List<MerchantDTO> findAllMerchant(int satrtPage, int pageSize,BigDecimal longitude,BigDecimal latitude);
+	// 分页倒叙查询商家
+	List<MerchantDTO> findAllMerchant(int satrtPage, int pageSize, BigDecimal longitude, BigDecimal latitude);
 
-    // (后台)商户列表
-    Result adminFindAllMerchant(String userid,String merchantName, String type,int satrtPage, int pageSize);
+	// (后台)商户列表
+	Result adminFindAllMerchant(String userid, String merchantName, String type, int satrtPage, int pageSize);
 
-    // 审批商户
-    Result judgeMerchant(Rewrite_JudgeMerchantDTO rewrite_JudgeMerchantDTO);
+	// 审批商户
+	Result judgeMerchant(Rewrite_JudgeMerchantDTO rewrite_JudgeMerchantDTO);
 }
